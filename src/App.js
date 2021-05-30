2270acea60f6f5dc9ad0ae72839ca8c5978cb918
ndjndj <teq.ndj@gmail.com>
2021-05-31 03:01:58 +0900

    axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import React from 'react';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+getJson = () => {
+  Axios.get(
+      'http://127.0.0.1:5000/json-test',
+      {params: params }
+  ).then(res =>{
+    console.log(res.data);
+  });
 }
+
 
 export default App;
