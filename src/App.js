a038caadad83626ab2db3d166a4b77b8db1d8ddd
ndjndj <teq.ndj@gmail.com>
2021-06-01 20:33:39 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import React from 'react';
 import Axios from 'axios';
 
-getJson = () => {
+const getJson = () => {
   Axios.get(
-      'http://127.0.0.1:5000/json-test',
-      {params: params }
+      'http://127.0.0.1:5000/json-test'
   ).then(res =>{
     console.log(res.data);
-  });
+  }).catch(e => {
+    console.log('error');
+    console.log(e);
+  })
+}
+
+class App extends React.Component {
+  componentDidMount() {
+    getJson();
+  }
+
+  render () {
+    return <h1>sample</h1>
+  }
+
 }
 
 
