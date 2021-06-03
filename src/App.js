744387ff51e6b9db14ca9c3c855bdfe25536b82c
ndjndj <teq.ndj@gmail.com>
2021-06-03 20:17:37 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -2,36 +2,27 @@ import './App.css';
 import React from 'react';
 import Axios from 'axios';
 
-const getJson = () => {
+const getJson = (state) => {
   Axios.get(
       'http://127.0.0.1:5000/json-test'
   ).then(res =>{
-    console.log(res.data);
+    state.setState({data: res.data});
     return res.data;
   }).catch(e => {
-    console.log('error');
-    console.log(e);
-    return null;
+    return [];
   })
 }
-
 class App extends React.Component {
-  state = {
-    data: []
+  constructor(props) {
+    super(props);
+    this.state = { data: [] }
   }
 
   componentDidMount() {
-    Axios.get(
-        'http://127.0.0.1:5000/json-test'
-    ).then(res =>{
-      console.log(res.data);
-      this.setState({ data: res.data });
-    }).catch(e => {
-      console.log('error');
-      console.log(e);
-      return null;
-    })
+    getJson(this.state);
   }
+
+
 
   render () {
     return <div>{this.state.data.map(d => <li>{d.farm}</li>)}</div>
